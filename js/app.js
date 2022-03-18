0d6cdda2ab0004418a088c472e8569d52895456d
Cascarr Ihesie <c.ihesie91@gmail.com>
2022-03-18 17:36:04 +0700

    first upload of color-flipper-bsic

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const colorArr = ["red", "#f15025", "green", "rgba(133, 122, 200)"];
+
+const resDply = document.querySelector(' .result');
+const btn = document.querySelector('button');
+const selBdy = document.querySelector('body')
+
+btn.addEventListener('click', function() {
+
+    const ranNumber = getRandomNumber();
+
+    selBdy.style.background = colorArr[ranNumber];
+
+    resDply.textContent = colorArr[ranNumber];
+});
+
+function getRandomNumber() {
+
+    return Math.floor(Math.random() * colorArr.length);
+}
